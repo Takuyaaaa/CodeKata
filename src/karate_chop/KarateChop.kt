a26feebf3ce7482c1716af5ba66278acbbead9fb
Takuyaaaa <xxtyzs@gmail.com>
2020-12-26 16:40:54 +0900

    blushed up iteratorChop method

--- src/karate_chop/KarateChop.kt
+++ src/karate_chop/KarateChop.kt
@@ -4,40 +4,58 @@ import kotlin.math.ceil
 
 class KarateChop {
     companion object {
-        // chop a list by iterating numbers
-        fun iteratorChop(targetNum: Int, list: List<Int>): Int {
-            if (list.isEmpty()) {
-                return -1
+        private const val NOT_FOUND = -1
+        /**
+         * chop a list by binary search with iterating
+         */
+        fun iteratorChop(targetNum: Int, originalList: List<Int>): Int {
+            /**
+             * calculate list`s index located at middle
+             */
+            fun List<Int>.calculateMiddleIndex(): Int {
+                return ceil(this.lastIndex.toDouble().div(2.0)).toInt()
             }
 
-            var middleIndex = ceil(list.lastIndex.toDouble().div(2.0)).toInt()
-            var middleNumber = list[middleIndex]
-            var modList = list
+            // no calculation needed if originalList is empty
+            if (originalList.isEmpty()) {
+                return NOT_FOUND
+            }
 
+            var middleIndex = originalList.calculateMiddleIndex()
+
+            // prepare a list which would be manipulated through the while process below
+            var manipulatedList = originalList
+
+            // keep processing until find targetNum or realize no targetNum
             while (true) {
-                if (middleNumber == targetNum) {
+                // number which is located at middle and will be compared to targetNum
+                val middleNum = originalList[middleIndex]
+
+                if (middleNum == targetNum) {
                     return middleIndex
                 }
 
-                if (modList.size == 1 && middleNumber != targetNum) {
-                    return -1
+                if (manipulatedList.size == 1 && middleNum != targetNum) {
+                    return NOT_FOUND
                 }
 
-                if (modList.size == 2 && modList[0] != targetNum && modList.last() != targetNum) {
-                    return -1
+                // when only two numbers left and neither of them are not matched to targetNum
+                if (manipulatedList.size == 2 &&
+                    manipulatedList[0] != targetNum && manipulatedList.last() != targetNum) {
+                    return NOT_FOUND
                 }
 
-                if (middleNumber < targetNum) {
-                    modList = modList.slice(middleIndex..modList.lastIndex)
-                    middleIndex += ceil(modList.lastIndex.toDouble().div(2.0)).toInt()
-                    middleNumber = list[middleIndex]
+                // when targetNum is bigger than middleNum, pick upper half as manipulatedList
+                if (middleNum < targetNum) {
+                    manipulatedList = manipulatedList.slice(middleIndex..manipulatedList.lastIndex)
+                    middleIndex += manipulatedList.calculateMiddleIndex()
                     continue
                 }
 
-                if (middleNumber > targetNum) {
-                    modList = modList.slice(0..middleIndex)
-                    middleIndex -= ceil(modList.lastIndex.toDouble().div(2.0)).toInt()
-                    middleNumber = list[middleIndex]
+                // when targetNum is smaller than middleNum, pick lower half as manipulatedList
+                if (middleNum > targetNum) {
+                    manipulatedList = manipulatedList.slice(0..middleIndex)
+                    middleIndex -= manipulatedList.calculateMiddleIndex()
                     continue
                 }
             }
