898bd39e5dda98546736c15e4dbb534d9ffe84f0
Takuyaaaa <xxtyzs@gmail.com>
2020-12-26 15:13:27 +0900

    implementing iterative binary search

--- src/karate_chop/KarateChop.kt
+++ src/karate_chop/KarateChop.kt
@@ -4,14 +4,37 @@ class KarateChop {
     companion object {
         // chop a list by iterating numbers
         fun iteratorChop(targetNum: Int, list: List<Int>): Int {
-            for ((index, number) in list.withIndex()) {
-                if (number == targetNum) {
-                    return index
-                }
+            if (list.isEmpty()) {
+                return -1
             }
 
-            // when targetNum not found, return -1
-            return -1
+            var middleIndex = list.lastIndex/2
+            var middleNumber = list[middleIndex]
+            var modList = list
+
+            while (true) {
+                if (middleNumber == targetNum) {
+                    return middleIndex
+                }
+
+                if (middleIndex == 0 || middleIndex == list.lastIndex && list[middleIndex] != targetNum) {
+                    return -1
+                }
+
+                if (middleNumber < targetNum) {
+                    val upperPart = list.slice(middleIndex..list.lastIndex)
+                    middleIndex += (upperPart.size / 2)
+                    middleNumber = list[middleIndex]
+                    continue
+                }
+
+                if (middleNumber > targetNum) {
+                    val lowerPart = list.slice(0..middleIndex)
+                    middleIndex = lowerPart.lastIndex/2
+                    middleNumber = list[middleIndex]
+                    continue
+                }
+            }
         }
     }
 }
