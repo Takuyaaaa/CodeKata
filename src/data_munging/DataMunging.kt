c6591db950a36f2ae2f5036c04621b0981dd3139
Takuyaaaa <xxtyzs@gmail.com>
2020-12-28 20:50:48 +0900

    refactor readFileAndPrintSmallestSpread method

--- src/data_munging/DataMunging.kt
+++ src/data_munging/DataMunging.kt
@@ -5,29 +5,43 @@ import java.io.File
 
 class DataMunging {
     companion object {
-        fun readFileAndReturnSmallestSpread() {
-            val dateWithSpread = File("resources/weather.dat")
-                    .bufferedReader()
-                    .readLines()
-                    .takeLast(31).dropLast(1)
-                    .map {
-                        // process string data to cleansed list data
-                        val line = it.replace("\\s+".toRegex(), " ")
-                                .replace("*", "")
-                                .split(" ")
-                                .drop(1)
+        fun readFileAndPrintSmallestSpread() {
+            /**
+             * process string data to cleansed list data
+             */
+            fun String.cleanseDataFormat(): List<String> {
+                return this.replace("\\s+".toRegex(), " ")
+                        .replace("*", "")
+                        .split(" ")
+                        .drop(1)
+            }
 
-                        // extract temperature data to calculate difference
-                        val date = line[0].toInt()
-                        val maxTemp = line[1].toInt()
-                        val minTemp = line[2].toInt()
-                        // map list with date and difference of maxTemp and MinTemp
-                        listOf(date, maxTemp.minus(minTemp))
-                    }
-                    // have smallest spread date as first element and pick it to print
-                    .sortedBy { it[1] }[0]
+            /**
+             * calculate smallest spread and return that date with spread value
+             */
+            fun File.calculateSmallestSpread(): Map<String, Int> {
+                return this.bufferedReader()
+                        .readLines()
+                        .takeLast(31).dropLast(1)
+                        .map {
+                            // process string data to cleansed list data
+                            val line = it.cleanseDataFormat()
 
-            println(dateWithSpread)
+                            val date = line[0].toInt()
+                            // extract temperature data to calculate spread
+                            val maxTemp = line[1].toInt()
+                            val minTemp = line[2].toInt()
+                            // make a map containing date and spread as keys
+                            mapOf("date" to date, "spread" to maxTemp.minus(minTemp))
+                        }
+                        // have smallest spread date as first element and pick it to print
+                        .sortedBy { it["spread"] }[0]
+            }
+
+            val smallestSpread = File("resources/weather.dat")
+                    .calculateSmallestSpread()
+
+            println(smallestSpread)
         }
     }
 }
