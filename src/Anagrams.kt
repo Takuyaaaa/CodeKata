c0345149eb5a25c19eef4e7decad92f83f8ca581
Takuyaaaa <xxtyzs@gmail.com>
2021-01-01 15:48:51 +0900

    implement anagrams algorithm

--- src/Anagrams.kt
+++ src/Anagrams.kt
@@ -1,4 +1,46 @@
 package com.code_kata
 
+import java.io.File
+
 class Anagrams {
+    companion object {
+        /**
+         * execute processes and print the most repeated anagrams
+         */
+        fun File.printMostRepeatedAnagram() {
+            this.addUsedWordsInfo()
+             // groupBy sorted string used for anagrams
+             .groupBy { it.first }
+             .pickMostRepeatedAnagram()
+             // print the most repeated anagrams by referring second element of each Pair
+             .forEach {
+                 println(it.second)
+             }
+        }
+
+        /**
+         * make pair: first is sorted string used for an anagram, second is raw string data
+         */
+        private fun File.addUsedWordsInfo(): List<Pair<String, String>> {
+            return this.readLines()
+                    .map {
+                        val sortedString = it.sort()
+                        Pair(sortedString, it)
+                    }
+        }
+
+        /**
+         * sort string alphabetically
+         */
+        private fun String.sort(): String {
+            return this.toCharArray().sorted().joinToString("")
+        }
+
+        /**
+         * return words categorised into the most repeated anagram
+         */
+        private fun Map<String, List<Pair<String, String>>>.pickMostRepeatedAnagram(): List<Pair<String, String>> {
+            return this.toList().sortedByDescending { it.second.count() }[0].second
+        }
+    }
 }
--- test/AnagramsTest.kt
+++ test/AnagramsTest.kt
@@ -1,19 +1,11 @@
+import com.code_kata.Anagrams.Companion.printMostRepeatedAnagram
 import org.junit.Test
 import java.io.File
 
 class AnagramsTest {
     @Test
     fun testAnagrams() {
-        val list = File("resources/wordlist_for_anagrams.txt")
-                .readLines()
-                .map {
-                    val chars = it.toLowerCase().toSortedSet()
-                    Pair(chars, it)
-                }
-                .groupBy { it.first }
-                .toList()
-                .sortedByDescending { it.second.count() }
-
-        println(list[0])
+        File("resources/wordlist_for_anagrams.txt")
+                .printMostRepeatedAnagram()
     }
 }
