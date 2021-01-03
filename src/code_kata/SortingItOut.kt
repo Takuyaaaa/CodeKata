ca70f5ca4fb08db8bb2bab5d595c24e86c2245a2
Takuyaaaa <xxtyzs@gmail.com>
2021-01-03 15:43:47 +0900

    implement SortedItOut method

--- src/code_kata/SortingItOut.kt
+++ src/code_kata/SortingItOut.kt
@@ -1,24 +1,57 @@
 package com.code_kata.code_kata
 
 class Rack {
+    // balls stored at Rack instance by "add" method
     val balls = mutableListOf<Int>()
 
+    /**
+     * store ball number at "balls" in the sorted order
+     */
     fun add(ball: Int) {
         balls.add(ball)
 
-        // balls.sort() will solve the problem, but...
+        // balls.sort() will do, but this time try to implement by scratch
         balls.bubbleSort()
     }
 
+    /**
+     * sort list by bubble sort algorithm
+     */
     private fun MutableList<Int>.bubbleSort() {
         this.mapIndexed { index, num ->
+            // from the bottom, compare a number to next number
             this.slice(index..this.lastIndex)
                     .reversed().forEach { newtNum ->
+                        // if number is bigger than next number, swap their positions
                         if (newtNum < num) {
                             this[index] = newtNum
                             this[index+1] = num
                         }
                     }
         }
+    }
+}
+
+
+class Sentence(private val sentence: String) {
+    /**
+     * make "sentence" sorted and remove punctuations
+     */
+    fun extractSortedChars(): String {
+        return sentence.replace(" ", "")
+                .removePunctuations()
+                .map { it.toLowerCase() }
+                // sort alphabetically
+                .sortedBy { it }
+                // make list to String
+                .joinToString("")
+    }
+
+    /**
+     * remove punctuations from String
+     */
+    private fun String.removePunctuations(): String {
+        return this.filter { it != ',' }
+                .filter { it != '.' }
     }
 }
--- test/code_kata/SortingItOutTest.kt
+++ test/code_kata/SortingItOutTest.kt
@@ -1,6 +1,7 @@
 package code_kata
 
 import com.code_kata.code_kata.Rack
+import com.code_kata.code_kata.Sentence
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -8,6 +9,7 @@ class SortingItOutTest {
     @Test
     fun testRuck() {
         val rack = Rack()
+
         assertEquals(mutableListOf(), rack.balls)
         rack.add(20)
         assertEquals(mutableListOf(20), rack.balls)
@@ -15,5 +17,17 @@ class SortingItOutTest {
         assertEquals(mutableListOf(10, 20), rack.balls)
         rack.add(30)
         assertEquals(mutableListOf(10, 20, 30), rack.balls)
+    }
+
+    @Test
+    fun testChars() {
+        val sentence = Sentence(
+                "When not studying nuclear physics, Bambi likes to play beach volleyball."
+        )
+        val sortedChars = sentence.extractSortedChars()
+
+        assertEquals(
+                "aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy",
+                sortedChars)
     }
 }
