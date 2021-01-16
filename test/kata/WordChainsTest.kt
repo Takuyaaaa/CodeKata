911f6b1bcbd92a04e0665c684860b24d54186675
Takuya Natsume <xxtyzs@gmail.com>
2021-01-16 16:22:31 +0900

    add assertions

--- test/kata/WordChainsTest.kt
+++ test/kata/WordChainsTest.kt
@@ -2,12 +2,17 @@ package kata
 
 import com.code_kata.kata.WordChains
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class WordChainsTest {
     @Test
     fun testWordChains1() {
         val wordChains = WordChains("cat", "dog")
         wordChains.executeWordChains()
+
+        assertEquals("cat", wordChains.wordResult.first())
+        assertEquals("dog", wordChains.wordResult.last())
+
         println(wordChains.wordResult)
     }
 
@@ -15,6 +20,10 @@ class WordChainsTest {
     fun testWordChains2() {
         val wordChains = WordChains("lead", "gold")
         wordChains.executeWordChains()
+
+        assertEquals("lead", wordChains.wordResult.first())
+        assertEquals("gold", wordChains.wordResult.last())
+
         println(wordChains.wordResult)
     }
 
@@ -22,6 +31,10 @@ class WordChainsTest {
     fun testWordChains3() {
         val wordChains = WordChains("ruby", "code")
         wordChains.executeWordChains()
+
+        assertEquals("ruby", wordChains.wordResult.first())
+        assertEquals("code", wordChains.wordResult.last())
+
         println(wordChains.wordResult)
     }
 }
